14c4316d52d421c31dcbbcf58c5ed46f8e2edc1e
johnnny <jazarewicz@gmail.com>
2016-06-21 18:23:17 +0200

    HighScore edit field name;

--- src/main/java/pl/johnny/gwtQuiz/client/ui/Contact.java
+++ src/main/java/pl/johnny/gwtQuiz/client/ui/Contact.java
@@ -6,10 +6,12 @@ public class Contact {
 	final String address;
 	final Date birthday;
 	final String name;
+	public boolean isThisCellEditable;
 
-	public Contact(String name, Date birthday, String address) {
+	public Contact(String name, Date birthday, String address, boolean isThisCellEditable) {
 		this.name = name;
 		this.birthday = birthday;
 		this.address = address;
+		this.isThisCellEditable = isThisCellEditable;
 	}
 }
--- src/main/java/pl/johnny/gwtQuiz/client/ui/HighScoreCellTableView.java
+++ src/main/java/pl/johnny/gwtQuiz/client/ui/HighScoreCellTableView.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 import org.gwtbootstrap3.client.ui.gwt.CellTable;
 
-import com.google.gwt.cell.client.DateCell;
-import com.google.gwt.cell.client.EditTextCell;
+import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.cell.client.TextInputCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.view.client.SelectionChangeEvent;
-import com.google.gwt.view.client.SingleSelectionModel;
+import com.google.gwt.view.client.NoSelectionModel;
 
 public class HighScoreCellTableView extends VerticalPanel {
 
@@ -22,36 +22,38 @@ public class HighScoreCellTableView extends VerticalPanel {
 	* The list of data to display.
 	*/
 	private static final List<Contact> CONTACTS = Arrays.asList(
-			new Contact("John", new Date(80, 4, 12), "123 Fourth Avenue"),
-			new Contact("Joe", new Date(85, 2, 22), "22 Lance Ln"),
-			new Contact("George", new Date(46, 6, 6), "1600 Pennsylvania Avenue"));
+			new Contact("John", new Date(80, 4, 12), "5", true),
+			new Contact("Joe", new Date(85, 2, 22), "4", false),
+			new Contact("George", new Date(46, 6, 6), "4", false));
 
 	public HighScoreCellTableView() {
 
 		CellTable<Contact> cellTableHighScores = new CellTable<Contact>();
 		cellTableHighScores.setWidth("100%", true);
 		cellTableHighScores.setStriped(true);
-//		cellTableHighScores.setCondensed(true);
-//		cellTableHighScores.setBordered(true);
-		cellTableHighScores.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+		//		cellTableHighScores.setCondensed(true);
+		//		cellTableHighScores.setBordered(true);
+				cellTableHighScores.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.DISABLED);
 
 		// Add a text column to show the name.
-		Column<Contact, String> nameColumn = new Column<Contact, String>(new EditTextCell()) {
+		Column<Contact, String> nameColumn = new Column<Contact, String>(new MyTextInputCell()) {
 			@Override
 			public String getValue(Contact object) {
 				return object.name;
 			}
 		};
-		cellTableHighScores.addColumn(nameColumn, "Name");
+		
+		nameColumn.setFieldUpdater( new FieldUpdater<Contact, String>(){
 
-		// Add a date column to show the birthday.
-		Column<Contact, Date> dateColumn = new Column<Contact, Date>(new DateCell()) {
 			@Override
-			public Date getValue(Contact object) {
-				return object.birthday;
+			public void update(int index, Contact object, String value) {
+				GWT.log("changed value " + value);	
 			}
-		};
-		cellTableHighScores.addColumn(dateColumn, "Birthday");
+
+			 
+			});
+		
+		cellTableHighScores.addColumn(nameColumn, "Player");
 
 		// Add a text column to show the address.
 		TextColumn<Contact> addressColumn = new TextColumn<Contact>() {
@@ -60,20 +62,11 @@ public class HighScoreCellTableView extends VerticalPanel {
 				return object.address;
 			}
 		};
-		cellTableHighScores.addColumn(addressColumn, "Address");
+		cellTableHighScores.addColumn(addressColumn, "Score");
 
 		// Add a selection model to handle user selection.
-		final SingleSelectionModel<Contact> selectionModel = new SingleSelectionModel<Contact>();
-		cellTableHighScores.setSelectionModel(selectionModel);
-		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
-			@Override
-			public void onSelectionChange(SelectionChangeEvent event) {
-				Contact selected = selectionModel.getSelectedObject();
-				if(selected != null) {
-					GWT.log("You selected: " + selected.name);
-				}
-			}
-		});
+				final NoSelectionModel<Contact> selectionModel = new NoSelectionModel<Contact>();
+				cellTableHighScores.setSelectionModel(selectionModel);
 
 		// Set the total row count. This isn't strictly necessary,
 		// but it affects paging calculations, so its good habit to
@@ -82,7 +75,19 @@ public class HighScoreCellTableView extends VerticalPanel {
 
 		// Push the data into the widget.
 		cellTableHighScores.setRowData(0, CONTACTS);
-	
+
 		add(cellTableHighScores);
+	}
+
+	public class MyTextInputCell extends TextInputCell {
+		@Override
+		public void render(Context context, String value, SafeHtmlBuilder sb) {
+			Contact object = (Contact) context.getKey();
+			if(object.isThisCellEditable) {
+				 super.render(context,value,sb);
+			} else {
+				sb.appendEscaped(value); // our some other HTML. Whatever you want.
+			}
+		}
 	}
 }
