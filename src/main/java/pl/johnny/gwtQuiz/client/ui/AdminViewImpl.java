4ea4b6df0a0ad3be97c4419e90f88063464045d2
johnnny333 <janzarewicz@gmail.com>
2016-09-29 19:28:49 +0200

    Category list styling;

--- src/main/java/pl/johnny/gwtQuiz/client/ui/AdminViewImpl.java
+++ src/main/java/pl/johnny/gwtQuiz/client/ui/AdminViewImpl.java
@@ -11,21 +11,19 @@ import org.gwtbootstrap3.client.ui.Alert;
 import org.gwtbootstrap3.client.ui.Icon;
 import org.gwtbootstrap3.client.ui.PanelGroup;
 import org.gwtbootstrap3.client.ui.constants.AlertType;
-import org.gwtbootstrap3.client.ui.gwt.CellTable;
 
-import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.TextInputCell;
+import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.ProvidesKey;
 
 import pl.johnny.gwtQuiz.client.ui.widgets.PanelWidget;
 import pl.johnny.gwtQuiz.shared.Question;
@@ -72,19 +70,18 @@ public class AdminViewImpl extends Composite implements AdminView {
 		Alert noQuestionAlert = new Alert("No user questions!");
 		noQuestionAlert.addStyleName("text-center");
 		noQuestionAlert.setType(AlertType.INFO);
-		if (tmpQuestion.size() == 0) {
+		if(tmpQuestion.size() == 0) {
 			panelGroup.clear();
 			panelGroup.add(noQuestionAlert);
 			return;
-		}
-		;
+		} ;
 
 		panelWidgets = new PanelWidget[tmpQuestion.size()];
 
 		// Avoid doubling widgets...
 		panelGroup.clear();
 
-		for (int i = 0; i < panelWidgets.length; i++) {
+		for(int i = 0; i < panelWidgets.length; i++) {
 
 			panelWidgets[i] = new PanelWidget(listener);
 			panelWidgets[i].setUserCategoryListBox(categories, tmpQuestion.get(i).getCategory());
@@ -92,22 +89,22 @@ public class AdminViewImpl extends Composite implements AdminView {
 			panelWidgets[i].setUserImage(tmpQuestion.get(i).getID(), tmpQuestion.get(i).getImageURL());
 			panelWidgets[i].setUserQuestionField(tmpQuestion.get(i).getQuestion());
 
-			for (int j = 0; j < tmpQuestion.get(i).getAnswers().length; j++) {
+			for(int j = 0; j < tmpQuestion.get(i).getAnswers().length; j++) {
 				panelWidgets[i].setUserAnswer1Field(tmpQuestion.get(i).getAnswer(j));
 
-				switch (j) {
-				case 0:
-					panelWidgets[i].setUserAnswer1Field(tmpQuestion.get(i).getAnswer(j));
-					break;
-				case 1:
-					panelWidgets[i].setUserAnswer2Field(tmpQuestion.get(i).getAnswer(j));
-					break;
-				case 2:
-					panelWidgets[i].setUserAnswer3Field(tmpQuestion.get(i).getAnswer(j));
-					break;
-				case 3:
-					panelWidgets[i].setUserAnswer4Field(tmpQuestion.get(i).getAnswer(j));
-					break;
+				switch(j) {
+					case 0:
+						panelWidgets[i].setUserAnswer1Field(tmpQuestion.get(i).getAnswer(j));
+						break;
+					case 1:
+						panelWidgets[i].setUserAnswer2Field(tmpQuestion.get(i).getAnswer(j));
+						break;
+					case 2:
+						panelWidgets[i].setUserAnswer3Field(tmpQuestion.get(i).getAnswer(j));
+						break;
+					case 3:
+						panelWidgets[i].setUserAnswer4Field(tmpQuestion.get(i).getAnswer(j));
+						break;
 				}
 			}
 
@@ -135,75 +132,42 @@ public class AdminViewImpl extends Composite implements AdminView {
 	}
 
 	public void buildCategoriesTable() {
-
 		/**
-		 * A simple data type that represents a contact with a unique ID.
-		 */
-		class Contact {
-			private int nextId = 0;
+		   * The list of data to display.
+		   */
+		final List<String> DAYS = Arrays.asList("Sunday", "Monday",
+				"Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 
-			private final int id;
-			private String name;
+		// Create a cell that will interact with a value updater.
+		TextInputCell inputCell = new TextInputCell();
 
-			public Contact(String name) {
-				nextId++;
-				this.id = nextId;
-				this.name = name;
-			}
-		}
-
-		/**
-		 * The list of data to display.
-		 */
-		final List<Contact> CONTACTS = Arrays.asList(new Contact("John"), new Contact("Joe"), new Contact("George"));
-
-		/**
-		 * The key provider that allows us to identify Contacts even if a field
-		 * changes. We identify contacts by their unique ID.
-		 */
-		final ProvidesKey<Contact> KEY_PROVIDER = new ProvidesKey<Contact>() {
+		// Create a CellList that uses the cell.
+		CellList<String> cellList = new CellList<String>(inputCell);
+		cellList.addStyleName("categories-table-container");
+		
+		// Create a value updater that will be called when the value in a cell
+		// changes.
+		ValueUpdater<String> valueUpdater = new ValueUpdater<String>() {
 			@Override
-			public Object getKey(Contact item) {
-				return item.id;
+			public void update(String newValue) {
+				Window.alert("You typed: " + newValue);
 			}
 		};
 
-		// Create a CellTable with a key provider.
-		final CellTable<Contact> table = new CellTable<Contact>(KEY_PROVIDER);
-		table.setStriped(true);
+		// Add the value updater to the cellList.
+		cellList.setValueUpdater(valueUpdater);
 
-		// Add a text input column to edit the name.
-		final TextInputCell nameCell = new TextInputCell();
-		Column<Contact, String> nameColumn = new Column<Contact, String>(nameCell) {
-			@Override
-			public String getValue(Contact object) {
-				// Return the name as the value of this column.
-				return object.name;
-			}
-		};
-		table.addColumn(nameColumn, "Name");
-
-		// Add a field updater to be notified when the user enters a new name.
-		nameColumn.setFieldUpdater(new FieldUpdater<Contact, String>() {
-			@Override
-			public void update(int index, Contact object, String value) {
-				// Inform the user of the change.
-				Window.alert("You changed the name of " + object.name + " to " + value);
-
-				// Push the changes into the Contact. At this point, you could
-				// send an
-				// asynchronous request to the server to update the database.
-				object.name = value;
-
-				// Redraw the table with the new data.
-				table.redraw();
-			}
-		});
+		// Set the total row count. This isn't strictly necessary, but it affects
+		// paging calculations, so its good habit to keep the row count up to date.
+		cellList.setRowCount(DAYS.size(), true);
 
 		// Push the data into the widget.
-		table.setRowData(CONTACTS);
-
+		cellList.setRowData(0, DAYS);
+		
+		GWT.log(DAYS.get(1));		
+		
 		// Add it to the root panel.
-		categoriesTableContainer.add(table);
+		categoriesTableContainer.add(cellList);
+
 	}
 }
